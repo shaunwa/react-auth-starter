25d5ffcc469eb2dc15adf14143eed3f18f916440
teomihsav <teomihsav76@gmail.com>
2022-12-31 18:58:39 +0200

    BE test

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import { routes } from './routes';
-import { initializeDbConnection } from './db';
+import express from "express";
+import { routes } from "./routes";
+import { initializeDbConnection } from "./db";
 
 const PORT = process.env.PORT || 8080;
 
@@ -12,16 +12,17 @@ app.use(express.json());
 
 // Add all the routes to our Express server
 // exported from routes/index.js
-routes.forEach(route => {
-    app[route.method](route.path, route.handler);
+routes.forEach((route) => {
+	app[route.method](route.path, route.handler);
 });
 
 // Connect to the database, then start the server.
 // This prevents us from having to create a new DB
 // connection for every request.
-initializeDbConnection()
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is listening on port ${PORT}`);
-        });
-    });
+
+// initializeDbConnection()
+//     .then(() => {
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
+});
+// });
