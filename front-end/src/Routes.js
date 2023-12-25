35b860673f737892a52d5ac178f9db83c3b3b90c
joelnyongesa <joelnyongesa148@gmail.com>
2023-12-25 20:54:47 +0300

    feat: added new sign up and log in routes

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -1,12 +1,21 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { UserInfoPage } from './pages/UserInfoPage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import PrivateRoute from './auth/PrivateRoute';
 
 export const Routes = () => {
     return (
         <Router>
             <Switch>
-                <Route path="/" exact>
+                <PrivateRoute path="/" exact>
                     <UserInfoPage />
+                </PrivateRoute>
+                <Route path="/login">
+                    <LoginPage />
+                </Route>
+                <Route path="/signup">
+                    <SignUpPage />
                 </Route>
             </Switch>
         </Router>
